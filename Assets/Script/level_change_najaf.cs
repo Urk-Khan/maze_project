85ece78a29103ea10ad8790289db5fc0d476f25a
Ubaid ur Rehman <urk.khan@outlook.com>
2022-10-25 17:55:00 +0500

    adding najaf's screens

---
+++ Assets/Script/level_change_najaf.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level_change_najaf : MonoBehaviour
+{
+    [SerializeField]
+    string tagName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == tagName)
+        {
+            SceneManager.LoadScene("Najaf_level_1");
+        }
+    }
+}
